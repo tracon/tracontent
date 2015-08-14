eb08829ce9fb50fe506b56d1610caaa787972c50
Santtu Pajukanta <santtu@pajukanta.fi>
2015-08-14 22:54:40 +0300

    enforce visible_from >= public_from

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 from django.contrib import admin
 from django import forms
 
@@ -6,13 +8,24 @@ from ckeditor.widgets import CKEditorWidget
 from .models import Page, BlogPost, Redirect, CommonFields
 
 
-
 class PageAdminForm(forms.ModelForm):
     body = forms.CharField(
         widget=CKEditorWidget(),
         label=CommonFields.body['verbose_name'],
         required=not CommonFields.body['blank'],
     )
+
+    def clean_visible_from(self):
+        public_from = self.cleaned_data.get('public_from')
+        visible_from = self.cleaned_data.get('visible_from')
+
+        if visible_from:
+            if not public_from:
+                raise forms.ValidationError(u'Jotta sivu voi olla näkyvissä, sen tulee myös olla julkinen')
+            elif public_from > visible_from:
+                raise forms.ValidationError(u'Sivun täytyy tulla julkiseksi viimeistään samalla hetkellä kun se tulee näkyväksi')
+
+        return visible_from
 
     class Meta:
         model = Page
