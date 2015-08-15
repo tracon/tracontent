dc44d6d846baae485548823ce38d467041810e82
Santtu Pajukanta <santtu@pajukanta.fi>
2015-08-15 16:01:58 +0300

    serve media files in devel

--- tracontent/urls.py
+++ tracontent/urls.py
@@ -1,12 +1,25 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
+
+
 admin.autodiscover()
 
-
-urlpatterns = patterns('',
-    url(r'', include('kompassi_oauth2.urls')),
+actual_patterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^ckeditor/', include('ckeditor.urls')),
     url(r'', include('content.urls')),
-)
+]
+
+if 'kompassi_oauth2' in settings.INSTALLED_APPS:
+    actual_patterns.append(
+        url(r'', include('kompassi_oauth2.urls'))
+    )
+
+if settings.DEBUG:
+    actual_patterns.extend(
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    )
+
+urlpatterns = patterns('', *actual_patterns)
