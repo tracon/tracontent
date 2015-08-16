b979175af366af8ab2b1ca4976d8e270186b8405
Santtu Pajukanta <santtu@pajukanta.fi>
2015-08-16 19:16:44 +0300

    fix oauth2

--- tracontent/urls.py
+++ tracontent/urls.py
@@ -9,10 +9,10 @@ admin.autodiscover()
 actual_patterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^ckeditor/', include('ckeditor.urls')),
-    url(r'', include('content.urls')),
 ]
 
 if 'kompassi_oauth2' in settings.INSTALLED_APPS:
+    print 'hello'
     actual_patterns.append(
         url(r'', include('kompassi_oauth2.urls'))
     )
@@ -21,5 +21,10 @@ if settings.DEBUG:
     actual_patterns.extend(
         static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     )
+
+# This needs to come last due to a catch-all route
+actual_patterns.append(
+    url(r'', include('content.urls')),
+)
 
 urlpatterns = patterns('', *actual_patterns)
