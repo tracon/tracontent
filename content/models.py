ccfec28546fc95b4eb3b04170b02951538d4a962
Santtu Pajukanta <santtu@pajukanta.fi>
2015-08-19 16:42:47 +0300

    forgot import

--- content/models.py
+++ content/models.py
@@ -5,6 +5,7 @@ from collections import namedtuple
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
+from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
 from django.core.validators import RegexValidator
 from django.db import models
