3e96e6c2e706b634f362190328ed1ca65a680b0e
Santtu Pajukanta <santtu@pajukanta.fi>
2015-09-07 22:10:33 +0300

    ads: fix banner redirect view

--- ads/views.py
+++ ads/views.py
@@ -6,7 +6,7 @@ from .models import BannerClick, Banner
 
 
 def ads_banner_redirect_view(request, banner_id):
-    banner = get_object_or_404(Banner, site=request.site, id=int(banner_id))
+    banner = get_object_or_404(Banner, sites=request.site, id=int(banner_id))
 
     if not request.user.is_staff:
         BannerClick.click(request.site, banner)
