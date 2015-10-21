9055af613ae54a836f53ee2fdb5df281208f7556
Santtu Pajukanta <santtu@pajukanta.fi>
2015-10-21 19:30:42 +0300

    hitpoint2015: improve front page layout

--- site_specific/hitpoint2015/context_processors.py
+++ site_specific/hitpoint2015/context_processors.py
@@ -10,10 +10,15 @@ def hitpoint2015_context(request):
     menu = site_settings.get_menu(t=now(), current_url=request.path)
 
     for subsite_path in ['/fi', '/en']:
+      print 'request.path', request.path
+      print 'subsite_path', subsite_path
       if request.path.startswith(subsite_path):
           subsite_frontpage = next(menu_entry for menu_entry in menu if menu_entry.href == subsite_path)
+          print subsite_frontpage
           menu = subsite_frontpage.children
+          break
       else:
+          print 'aieee'
           subsite_frontpage = None
 
     return dict(
