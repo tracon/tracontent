85dde460d79341aa9b1522052bf7948035f011e3
Santtu Pajukanta <santtu@pajukanta.fi>
2015-10-22 21:30:21 +0300

    content/admin: fix 500 on page creation related to previous commit

--- content/admin.py
+++ content/admin.py
@@ -89,9 +89,11 @@ class PageAdminForm(forms.ModelForm, CommonAdminFormMixin):
     def __init__(self, *args, **kwargs):
         forms.ModelForm.__init__(self, *args, **kwargs)
 
-        if self.instance and self.instance.site:
-            self.fields['parent'].queryset = self.instance.site.page_set.all()
-
+        if self.instance:
+            try:
+                self.fields['parent'].queryset = self.instance.site.page_set.all()
+            except Site.DoesNotExist:
+                pass
 
     def clean_site(self):
         site = self.cleaned_data.get('site')
