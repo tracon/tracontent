62a85015e1476bb5bfb9c44b9ac16eb17fe37422
Santtu Pajukanta <santtu@pajukanta.fi>
2015-11-05 00:24:56 +0200

    aicon: controller for organizer page

--- site_specific/aicon/views.py
+++ site_specific/aicon/views.py
@@ -1,9 +1,19 @@
 from content.models import SiteSettings
 
+from .models import Organizer
+
 
 def front_page_controller(request, page, num_blog_posts=5):
     current_site_settings = request.site.site_settings
 
     return dict(
         news_posts=current_site_settings.get_visible_blog_posts()[:num_blog_posts],
+    )
+
+
+def organizers_page_controller(request):
+    organizers = Organizer.objects.all()
+
+    return dict(
+        organizers=organizers,
     )
