ea6eec1ea82c8397a71b4fb5cbb79576d380cb52
Santtu Pajukanta <santtu@pajukanta.fi>
2015-11-14 00:52:10 +0200

    aicon: in admin, also make clickable those organizers that have no last name

--- site_specific/aicon/admin.py
+++ site_specific/aicon/admin.py
@@ -5,6 +5,7 @@ from .models import Organizer
 
 class OrganizerAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'nick', 'job_title', 'email')
+    list_display_links = ('last_name', 'first_name')
 
 
 admin.site.register(Organizer, OrganizerAdmin)
