bbd4595058c3e56dc980987aaa6d4a29eb257b0b
Santtu Pajukanta <santtu@pajukanta.fi>
2015-11-19 22:10:37 +0200

    resources: in admin, show create/update times for resources

--- resources/admin.py
+++ resources/admin.py
@@ -8,13 +8,25 @@ from .models import (
 )
 
 
+RESOURCE_ADMIN_FIELDSETS = (
+    (u'Sisältö', dict(
+        fields=('name', 'content', 'active'),
+    )),
+    (u'Lisätiedot', dict(
+        fields=('created_at', 'updated_at'),
+    )),
+)
+
+
 class StyleSheetAdmin(VersionAdmin):
     list_display = ('name', 'active')
-
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = RESOURCE_ADMIN_FIELDSETS
 
 class TemplateAdmin(VersionAdmin):
     list_display = ('name', 'active')
-
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = RESOURCE_ADMIN_FIELDSETS
 
 admin.site.register(Template, TemplateAdmin)
 admin.site.register(StyleSheet, StyleSheetAdmin)
